011cf06f8740b4df645e8305212bfaa84d69dda8
MathiasDB2001 <mathiasdebaere4@gmail.com>
2024-09-30 11:37:43 +0200

    new commit

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,8 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-
+        "views/estate_property_views.xml",
+        "security/ir.model.access.csv"
     ],
     "installable": True,
     'license': 'LGPL-3',
--- estate/models/models.py
+++ estate/models/models.py
@@ -1,15 +1,15 @@
 from odoo import fields, models
 
 class EstateProperty(models.Model):
-    _name = "Estate Property"
+    _name = "Estate.Property"
     _description = "description"
 
 
     name = fields.Char(required=True)
-    postcode = fields.Char(required=True)
+    postcode = fields.Char()
     date_availability = fields.Date()
     expected_price = fields.Float()
-    selling_price = fields.Float()
+    selling_price = fields.Float(required=True)
     bedrooms = fields.Integer()
     living_area = fields.Integer()
     facades = fields.Integer()
