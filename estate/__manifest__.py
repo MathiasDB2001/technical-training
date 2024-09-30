5297a2c33cc4b39cf2fc8a4cc11b53c494b592c2
MathiasDB2001 <mathiasdebaere4@gmail.com>
2024-09-30 11:43:50 +0200

    new commit

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,7 @@
     "depends": ["base"],  # dependencies
     "data": [
         "views/estate_property_views.xml",
+        "views/estate_menus.xml",
         "security/ir.model.access.csv"
     ],
     "installable": True,
