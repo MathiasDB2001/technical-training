cd697ad96cc2bcbb9676dd4658fc62b55b22f191
MathiasDB2001 <mathiasdebaere4@gmail.com>
2024-10-01 09:06:57 +0200

    new commit2

--- estate/models/property_tag.py
+++ estate/models/property_tag.py
@@ -1,8 +1,7 @@
 from odoo import fields, models
 
 class PropertyTag(models.Model):
-    _name='estate.property.Tag'
+    _name='estate.property.tag'
     _description = 'Estate Property Tag'
 
     name = fields.Char(string='Property Tag')
-    
